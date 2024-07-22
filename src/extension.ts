1bf683dcca011dc6e37c17b55416dbc445cb2a68
fengwmi <1980385456@qq.com>
2024-07-23 00:34:45 +0800

    feat: 新增json2go;优化部分代码

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,19 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import convertJsonToJava from './libs/convertJsonToJavaClass';
+import convertJsonToGo from './libs/convertJsonToGoLangClass';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('json2any.convertJsonToJava', () => {
+    registerConvertCommand(context, 'json2any.convertJsonToJava', convertJsonToJava, 'java');
+    registerConvertCommand(context, 'json2any.convertJsonToGolangClass', convertJsonToGo, 'go');
+
+    console.log('Congratulations, your extension "json2any" is now active!');
+}
+
+function registerConvertCommand(context: vscode.ExtensionContext, functionName: string, convertFunction: (jsonContent: string) => string, languageType: string) {
+    let disposable = vscode.commands.registerCommand(functionName, () => {
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             vscode.window.showInformationMessage('Open a JSON file first.');
@@ -15,27 +23,14 @@ export function activate(context: vscode.ExtensionContext) {
 
         const jsonContent = editor.document.getText();
         try {
-            const javaClass = convertJsonToJava(jsonContent);
-            vscode.workspace.openTextDocument({ content: javaClass, language: 'java' })
+            const convertedContent = convertFunction(jsonContent);
+            vscode.workspace.openTextDocument({content: convertedContent, language: languageType})
                 .then(doc => vscode.window.showTextDocument(doc));
         } catch (error) {
-            vscode.window.showErrorMessage('Failed to convert JSON to Java.');
+            vscode.window.showErrorMessage(`Failed to convert JSON to ${languageType}.`);
         }
     });
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "json2any" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// const disposable = vscode.commands.registerCommand('json2any.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from json2any!');
-	// });
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 
